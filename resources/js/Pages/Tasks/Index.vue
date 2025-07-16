23649c1e1ca9b76735c5ed61f7b394f6007ebe8f
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-07-16 14:46:58 +0700

    feat(tasks): change list to table

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -8,35 +8,47 @@ import { Inertia } from '@inertiajs/inertia';
 
 
   function destroy(id) {
-    if (confirm('Are you sure you want to delete this task?')) {
-      Inertia.delete(`/tasks/${id}`, {
-      onSuccess: () => {
-        console.log('Deleted!');
-      }
-    });
-  }
+     if (confirm('Are you sure you want to delete this task?')) {
+       Inertia.delete(`/tasks/${id}`, {
+       onSuccess: () => {
+         console.log('Deleted!');
+       }
+      });
     }
-    </script>
+  }
+</script>
 
 <template>
-  <div>
-    <h1 class="text-xl font-bold mb-4">Tasks</h1>
-    <div v-if="flash?.success" class="bg-green-100 border border-green-400 text-green-700 px-4 py-2 rounded mb-4">
-      {{ flash.success }}
+   <div>
+    <h1 class="text-2xl font-bold mb-6">Tasks</h1>
+    <div class="mb-4">
+      <a href="/tasks/create" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">+ Add New</a>
     </div>
-    <a href="/tasks/create" class="text-blue-500 underline">+ Add New</a>
-    <ul class="mt-4 space-y-2">
-      <li v-for="task in tasks" :key="task.id" class="border p-2">
-         <div>
-          {{ task.title }} ({{ task.description }})
-        </div>
-        <div class="space-x-2 mt-2">
-          <a :href="`/tasks/${task.id}`" class="text-blue-500 underline">Show</a>
-          <a :href="`/tasks/${task.id}/edit`" class="text-yellow-500 underline">Edit</a>
-          <button @click="destroy(task.id)" class="text-red-500 underline">Delete</button>
-        </div>
-      </li>
-    </ul>
+
+    <div class="overflow-x-auto bg-white shadow rounded-lg">
+      <table class="min-w-full table-auto border border-gray-200">
+        <thead class="bg-gray-100">
+          <tr>
+            <th class="px-4 py-2 border-b text-left">#</th>
+            <th class="px-4 py-2 border-b text-left">Title</th>
+            <th class="px-4 py-2 border-b text-left">Description</th>
+            <th class="px-4 py-2 border-b text-left">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(task, index) in tasks" :key="task.id" class="hover:bg-gray-50">
+            <td class="px-4 py-2 border-b">{{ index + 1 }}</td>
+            <td class="px-4 py-2 border-b">{{ task.title }}</td>
+            <td class="px-4 py-2 border-b">{{ task.description }}</td>
+            <td class="px-4 py-2 border-b space-x-2">
+              <a :href="`/tasks/${task.id}`" class="text-blue-600 hover:underline">Show</a>
+              <a :href="`/tasks/${task.id}/edit`" class="text-yellow-600 hover:underline">Edit</a>
+              <button @click="destroy(task.id)" class="text-red-600 hover:underline">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
